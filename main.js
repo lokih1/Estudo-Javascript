8762c523d431280271c75b2eee45452fe09b5d7b
lokih1 <lokihya@gmail.com>
2023-11-13 17:19:33 -0300

    utilizando for

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
 function tocaSom(idTagAudio) {
   document.querySelector(idTagAudio).play();
+
 }
+
 const listaTeclado = document.querySelectorAll(".tecla");
 
-let contador = 0;
-
-while (contador < listaTeclado.length) {
+//CONDIÇÃO FOR
+for (let contador = 0; contador < listaTeclado.length; contador++) {
   const tecla = listaTeclado[contador];
   const somTeclado = tecla.classList[1];
 
-  const idSom = `#som_${somTeclado}`;
+  const idSom = `#som_${somTeclado}`; //temolate string {somTeclado}
+
   console.log(somTeclado);
 
   tecla.onclick = function () {
+
     tocaSom(idSom);
   };
 
-  contador = contador + 1;
-
   console.log(contador);
+
 }
+
+// CONDIÇÃO WHILE
+// while (contador < listaTeclado.length) {
+//   const tecla = listaTeclado[contador];
+//   const somTeclado = tecla.classList[1];
+
+//   const idSom = `#som_${somTeclado}`;
+//   console.log(somTeclado);
+
+//   tecla.onclick = function () {
+//     tocaSom(idSom);
+//   };
+
+//   contador = contador + 1;
+
+//   console.log(contador);
+// }
