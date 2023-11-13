7ac4f601109c5a5a63b4ba6749a9411f25fc5f4f
lokih1 <lokihya@gmail.com>
2023-11-13 16:00:33 -0300

    Estudo com while, referencia, parametro e template string

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-function somPom() {
-  document.querySelector("#som_tecla_pom").play();
+function tocaSom(idTagAudio) {
+  document.querySelector(idTagAudio).play();
 }
 const listaTeclado = document.querySelectorAll(".tecla");
 
 let contador = 0;
 
 while (contador < listaTeclado.length) {
-  listaTeclado[contador].onclick = somPom;
+  const tecla = listaTeclado[contador];
+  const somTeclado = tecla.classList[1];
+
+  const idSom = `#som_${somTeclado}`;
+  console.log(somTeclado);
+
+  tecla.onclick = function () {
+    tocaSom(idSom);
+  };
+
   contador = contador + 1;
 
   console.log(contador);
